05ccd42e5845efa37c8bfa5ca94bba9f0bd59974
aarti_sharma2023 <aarti.sharma@dotclear.io>
2026-01-30 21:34:56 +0530

    deploy on server

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
+    | Application Nam jh
     |--------------------------------------------------------------------------
     |
     | This value is the name of your application, which will be used when the
